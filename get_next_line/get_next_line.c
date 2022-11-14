02298f582652ae6f1a640363ee9853b4683f72f2
Lucas Bordonal <lbordonal@gmail.com>
2022-11-14 09:33:13 +0000

    14/11/22 - 09:33

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: lbordona <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/11 15:20:19 by lbordona          #+#    #+#             */
-/*   Updated: 2022/11/11 16:03:58 by lbordona         ###   ########.fr       */
+/*   Updated: 2022/11/14 09:32:27 by lbordona         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,26 @@
 /* char	*get_next_line(int fd)
 {
 
+} */
+
+/* int	main(void)
+{
+	int	fd;
+	int	i;
+	char	*line;
+
+	fd = open("tests/test1.txt", O_RDONLY);
+	i = 0;
+	while (i < 15)
+	{
+		line = get_next_line(fd);
+		printf("%s", "line ");
+		printf("%d\n", i);
+		printf("%s", line);
+		if (line == NULL)
+			break;
+		free(line);
+		i++;
+	}
+	return (0);
 } */
