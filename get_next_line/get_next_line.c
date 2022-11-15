b289fbfa4df12819e99fe61089f957a70bb7babc
Lucas Henrique Bordonal <lbordona@c1r9s1.42porto.com>
2022-11-15 16:18:16 +0000

    15/11/22 - 16:18

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: lbordona <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/11 15:20:19 by lbordona          #+#    #+#             */
-/*   Updated: 2022/11/15 15:51:28 by lbordona         ###   ########.fr       */
+/*   Updated: 2022/11/15 15:54:32 by lbordona         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -93,25 +93,3 @@ char	*ft_save(char *save)
 	free(save);
 	return (str);
 }
-
-/* int	main(void)
-{
-	int		fd;
-	int		i;
-	char	*line;
-
-	fd = open("../tests/hp.txt", O_RDONLY);
-	fd = open("../tests/test1.txt", O_RDONLY);
-	fd = open("../tests/big_line_with_nl", O_RDONLY);
-	fd = open("../tests/big_line_no_nl", O_RDONLY);
-	i = 1;
-	while (i < 20)
-	{
-		line = get_next_line(fd);
-		printf("line [%02d]: %s", i, line);
-		free(line);
-		i++;
-	}
-	close(fd);
-	return (0);
-} */
--- tests/main.c
+++ tests/main.c
@@ -6,11 +6,11 @@
 /*   By: lbordona <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 13:33:12 by lbordona          #+#    #+#             */
-/*   Updated: 2022/11/14 16:59:04 by lbordona         ###   ########.fr       */
+/*   Updated: 2022/11/15 15:54:25 by lbordona         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "../get_next_line/get_next_line.h"
 
 int	main(void)
 {
@@ -19,16 +19,14 @@ int	main(void)
 	char	*line;
 
 	fd = open("../tests/hp.txt", O_RDONLY);
+	//fd = open("../tests/test1.txt", O_RDONLY);
+	//fd = open("../tests/big_line_with_nl", O_RDONLY);
+	//fd = open("../tests/big_line_no_nl", O_RDONLY);
 	i = 1;
-	while (i < 150)
+	while (i < 1000)
 	{
 		line = get_next_line(fd);
-		printf("%s", "line ");
-		printf("%d", i);
-		printf("%s", ": ");
-		printf("%s", line);
-		if (!line[fd])
-			return (0);
+		printf("line [%02d]: %s", i, line);
 		free(line);
 		i++;
 	}
