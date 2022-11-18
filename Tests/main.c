99c8a741ced1f8de4a67c90c8d253ea91149ff5e
Lucas Bordonal <lbordonal@gmail.com>
2022-11-18 11:43:16 +0000

    18/11/22 - 11:43

--- Tests/main.c
+++ Tests/main.c
@@ -6,7 +6,7 @@
 /*   By: lbordona <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 13:33:12 by lbordona          #+#    #+#             */
-/*   Updated: 2022/11/17 14:46:00 by lbordona         ###   ########.fr       */
+/*   Updated: 2022/11/18 11:41:09 by lbordona         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,12 @@ int	main(void)
 	int		i;
 	char	*line;
 
-	//fd = open("../Tests/files/miranha.txt", O_RDONLY);
-	//fd = open("../Tests//files/simpsons.txt", O_RDONLY);
+/* 	fd = open("../Tests/files/miranha.txt", O_RDONLY);
+	fd = open("../Tests//files/simpsons.txt", O_RDONLY); */
 	fd = open("../Tests/files/hp.txt", O_RDONLY);
-	//fd = open("../Tests/files/hltv.txt", O_RDONLY);
-	//fd = open("../Tests/files/big_line_with_nl", O_RDONLY);
-	//fd = open("../Tests/files/big_line_no_nl", O_RDONLY);
+/* 	fd = open("../Tests/files/hltv.txt", O_RDONLY);
+	fd = open("../Tests/files/big_line_with_nl", O_RDONLY);
+	fd = open("../Tests/files/big_line_no_nl", O_RDONLY); */
 	i = 1;
 	while ((line = get_next_line(fd)))
 	{
